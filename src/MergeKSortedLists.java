3b5d5efd07e909fe778901fcfa651d33d5ba6dfc
zhangxinyin93 <zxyatbeijing@gmail.com>
2017-04-06 17:11:36 -0400

    Add divided and conquer for this problem

--- src/MergeKSortedLists.java
+++ src/MergeKSortedLists.java
@@ -9,7 +9,8 @@ import java.util.PriorityQueue;
  * @return [-1->2->4->null]
  */
 public class MergeKSortedLists {
-    public ListNode mergeKList(List<ListNode> lists) {
+
+    public ListNode mergeByHeap(List<ListNode> lists) {
         if(lists == null || lists.size() == 0) {
             return null;
         }
@@ -26,12 +27,63 @@ public class MergeKSortedLists {
                 head = head.next;
             }
         }
-        ListNode mergedHead = minHeap.remove();
+        ListNode mergedHead = new ListNode(0);
         ListNode dummy = mergedHead;
         while(!minHeap.isEmpty()) {
             mergedHead.next = minHeap.remove();
             mergedHead = mergedHead.next;
         }
-        return dummy;
+        return dummy.next;
+    }
+
+    /**
+     * Using Divided and Conquer to solve this
+     * @param lists
+     *          Lists of head of sorted lists
+     * @return merged head of sorted lists
+     */
+    public ListNode dividAndConquer(List<ListNode> lists) {
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        return mergeKListsHelper(lists, 0 ,lists.size() - 1);
+    }
+
+    private ListNode mergeKListsHelper(List<ListNode> lists, int start, int end) {
+        if (start == end) {
+            return lists.get(start);
+        }
+
+        // Divided
+        int mid = start + (end - start) / 2;
+
+        // Conquer
+        ListNode left = mergeKListsHelper(lists, start, mid);
+        ListNode right = mergeKListsHelper(lists, mid + 1, end);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode pointer = dummy;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                pointer.next = left;
+                left = left.next;
+            } else {
+                pointer.next = right;
+                right = right.next;
+            }
+        }
+
+        if (left != null) {
+            pointer.next = left;
+        } else {
+            pointer.next = right;
+        }
+
+        return dummy.next;
     }
 }
